9be92646c05f92218ba3c9528e947bd56b9f0132
prakarn-pc <prakarnwongsanit@gmail.com>
2025-10-31 07:17:10 +0700

    Fix test file

--- src/test/java/com/programoo/myapi/HelloServiceIntegrationTest.java
+++ src/test/java/com/programoo/myapi/HelloServiceIntegrationTest.java
@@ -1,9 +1,7 @@
 package com.programoo.myapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-//src/test/java/com/example/demo/HelloServiceIntegrationTest.java
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
