426727d592737d4f0b669f3c35fdf9ee9f5afdab
prakarn-pc <prakarnwongsanit@gmail.com>
2025-11-08 11:27:58 +0700

    Support deployment with tomcat

--- src/main/java/com/programoo/myapi/MyApiApplication.java
+++ src/main/java/com/programoo/myapi/MyApiApplication.java
@@ -2,9 +2,18 @@ package com.programoo.myapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyApiApplication {
+public class MyApiApplication extends SpringBootServletInitializer{
+
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyApiApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MyApiApplication.class, args);
     }
